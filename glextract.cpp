e44f4a16fc631c92f71f6bf6bf10b2d26e80107f
degarashi <slice013@gmail.com>
2015-06-13 17:45:49 +0900

    各エクスポートルーチンをmainから分離

--- glextract.cpp
+++ glextract.cpp
@@ -55,6 +55,7 @@
 #include <fstream>
 const static int MAX_FILESIZE = 0x800000;
 std::string ReadAll(std::istream& st) {
+	auto pos = st.tellg();
 	size_t sz = static_cast<size_t>(st.seekg(0, std::ios::end).tellg());
 	st.seekg(0, std::ios::beg);
 	if(sz > MAX_FILESIZE)
@@ -63,6 +64,8 @@ std::string ReadAll(std::istream& st) {
 		std::string ret;
 		ret.resize(sz);
 		st.read(&ret[0], sz);
+
+		st.seekg(pos);
 		return std::move(ret);
 	}
 	return std::string();
@@ -138,7 +141,159 @@ bool CheckArgs(int argc, char* arg[]) {
 	return true;
 }
 #include <unordered_set>
+using StringSet = std::unordered_set<std::string>;
+StringSet g_funcsInFile,
+		  g_constsInFile;
+void FindResource(std::fstream& fs) {
+	std::string oBuff = ReadAll(fs);
+	smatch res;
+	auto fnAddName = [&oBuff, &res](auto& m, const auto& re, int idx){
+		auto itr = oBuff.cbegin(),
+			itrE = oBuff.cend();
+		while(regex_search(itr, itrE, res, re)) {
+			m.insert(res[idx].str());
+			itr = res.suffix().first;
+		}
+	};
+	fnAddName(g_funcsInFile, re_gldefine, 1);
+	fnAddName(g_funcsInFile, re_gldefmethod, 1);
+	fnAddName(g_constsInFile, re_gldefconst, 1);
+}
+struct CountNum {
+	int count,
+		skipcount;
 
+	CountNum& operator += (const CountNum& c) {
+		count += c.count;
+		skipcount += c.skipcount;
+		return *this;
+	}
+};
+CountNum ExportConstValue(std::ostream& os, const std::string& buff) {
+	CountNum c{};
+	auto itr = buff.cbegin(),
+		 itrE = buff.cend();
+	smatch res;
+	for(;;) {
+		if(!regex_search(itr, itrE, res, re_define))
+			break;
+		itr = res.suffix().first;
+		// 既に出力した定義名ならばスキップ
+		const auto &name = res.str(1);
+		if(g_constsInFile.count(name) == 0) {
+			g_constsInFile.insert(name);
+			const auto &value = res.str(2);
+			os << "DEF_GLCONST(" << name << ", 0x" << value << ')' << std::endl;
+			++c.count;
+		} else
+			++c.skipcount;
+	}
+	return c;
+}
+void ExportDefinition(std::ostream& os, const Func& func) {
+	std::string funcDef;
+	funcDef.resize(func.name.size());
+	// :大文字変換
+	std::transform(func.name.cbegin(), func.name.cend(), funcDef.begin(), ::toupper);
+	std::stringstream ss;
+	ss << "PFN" << funcDef << "PROC";
+	// タイプ定義のアウトプット
+	os << "GLDEFINE(" + func.name + ',' + ss.str() + ')' << std::endl;
+}
+void ExportMethod(std::ostream& os, const Func& func) {
+	// ret_typeがvoidなら0, それ以外は1を出力
+	int bVoid = (func.ret_type == "void") ? 0 : 1;
+	os << "DEF_GLMETHOD(" << func.ret_type << ", " << bVoid << ", " << func.name << ", ";
+	if(func.arg_pair.empty())
+		os << "(), ()";
+	else {
+		if(func.arg_pair[0].type_name == "void")
+			os << "(), ()";
+		else {
+			for(auto& ap : func.arg_pair)
+				os << "(" << ap.type_name << ")";
+			os << ", ";
+			for(auto& ap : func.arg_pair)
+				os << "(" << ap.name << ")";
+		}
+	}
+	os << ')' << std::endl;
+}
+using ExportFunc = std::function<void (std::ostream&, const Func&)>;
+using ExportFuncV = std::vector<ExportFunc>;
+CountNum IterateFunctionDefinition(std::ostream& os, std::ifstream& def, const std::string& buff, const ExportFuncV& fl) {
+	CountNum count{};
+	smatch res;
+	auto itr = buff.cbegin(),
+		itrE = buff.cend();
+	// 関数出力は別途用意したマクロ記述で範囲を制限
+	std::string strMacro,
+				strReBegin,
+				strReEnd;
+	for(;;) {
+		if(def.eof())
+			break;
+		std::getline(def, strMacro);		// マクロ名
+		std::getline(def, strReBegin);		// RegEx構文(Begin)
+		std::getline(def, strReEnd);		// RegEx構文(End)
+		if(strMacro.empty() || strReBegin.empty() || strReEnd.empty())
+			break;
+		regex re_begin(strReBegin),
+				re_end(strReEnd);
+		for(;;) {
+			// 定義ファイルの記述に合った箇所まで読み飛ばし -> (読み込み始める地点)
+			if(!regex_search(itr, itrE, res, re_begin))
+				break;
+			itr = res.suffix().first;
+
+			// 定義の終わりを検出 -> (読み込み終わりの地点)
+			if(!regex_search(itr, itrE, res, re_end)) {
+				// 見つからない場合はエラー
+				throw std::runtime_error("no matching definition of end of range");
+			}
+
+			bool bDef = true;
+			auto itrLE = res.suffix().first;
+			for(;;) {
+				// プロトタイプ宣言の名前を取得
+				if(!regex_search(itr, itrLE, res, re_proto))
+					break;
+				itr = res.suffix().first;
+
+				// 既に出力した定義名ならばスキップ
+				Func func;
+				func.name = res.str(2);
+				func.ret_type = res.str(1);
+				if(g_bAppend && g_funcsInFile.count(func.name)!=0) {
+					++count.skipcount;
+					continue;
+				}
+				++count.count;
+				if(bDef) {
+					bDef = false;
+					// マクロ名を書き出す
+					os << "#ifdef " << strMacro << std::endl;
+				}
+
+				g_funcsInFile.insert(func.name);
+				{
+					auto itr = res[3].first,
+						itrE = res[3].second;
+					smatch res2;
+					while(regex_search(itr, itrE, res2, re_args)) {
+						func.arg_pair.push_back(ArgPair{res2.str(1), res2.str(2)});
+						itr = res2.suffix().first;
+					}
+				}
+				for(auto& f : fl)
+					f(os, func);
+			}
+			if(!bDef)
+				os << "#endif" << std::endl;
+		}
+	}
+	return count;
+}
 int main(int argc, char* arg[]) {
 	if(!CheckArgs(argc, arg)) {
 		std::cout << "usage: glextract [-adm] [extraction definition file] [OpenGL Header(typically, glext.h)] [output filename]" << std::endl;
@@ -146,7 +301,6 @@ int main(int argc, char* arg[]) {
 	}
 	if(g_exp == 0)
 		g_exp = Exp_All;
-
 	try {
 		// 入力ファイル内容を全部メモリにコピー
 		std::ifstream ifs(g_arg[Arg_Input]);
@@ -154,7 +308,6 @@ int main(int argc, char* arg[]) {
 			throw std::runtime_error("can't open input-file");
 
 		std::string buff = ReadAll(ifs);
-
 		// OpenGL API定義の検出に使うRegex構文を読み込む
 		std::ifstream def(g_arg[Arg_Define]);
 		if(!def.is_open())
@@ -175,149 +328,31 @@ int main(int argc, char* arg[]) {
 				throw std::runtime_error("can't open output-file");
 		}
 
-		smatch res;
-		std::unordered_set<std::string>	funcsInFile,
-										constsInFile;
-		//! 出力先ファイルに含まれる関数名を登録
-		if(g_bAppend) {
-			std::string oBuff = ReadAll(ofs);
-			auto fnAddName = [&oBuff, &res](auto& m, const auto& re, int idx){
-				auto itr = oBuff.cbegin(),
-					itrE = oBuff.cend();
-				while(regex_search(itr, itrE, res, re)) {
-					m.insert(res[idx].str());
-					itr = res.suffix().first;
-				}
-			};
-			fnAddName(funcsInFile, re_gldefine, 1);
-			fnAddName(funcsInFile, re_gldefmethod, 1);
-			fnAddName(constsInFile, re_gldefconst, 1);
-		}
+		// 出力先ファイルに含まれる関数名を登録
+		if(g_bAppend)
+			FindResource(ofs);
 		ofs.seekp(0, std::ios::end);
 
-		int count = 0,
-			skipcount = 0;
+		CountNum count{};
 		// ---------------- Const変数の検索と出力 ----------------
-		if(g_exp & Exp_Const) {
-			auto itr = buff.cbegin(),
-				 itrE = buff.cend();
-			for(;;) {
-				if(!regex_search(itr, itrE, res, re_define))
-					break;
-				itr = res.suffix().first;
-				// 既に出力した定義名ならばスキップ
-				const auto &name = res.str(1);
-				if(constsInFile.count(name) == 0) {
-					constsInFile.insert(name);
-					const auto &value = res.str(2);
-					ofs << "DEF_GLCONST(" << name << ", 0x" << value << ')' << std::endl;
-					++count;
-				} else
-					++skipcount;
-			}
-		}
+		if(g_exp & Exp_Const)
+			count += ExportConstValue(ofs, buff);
 		// ---------------- Definition, Method の検索と出力 ----------------
-		auto itr = buff.cbegin(),
-			itrE = buff.cend();
-		// 関数出力は別途用意したマクロ記述で範囲を制限
-		std::string strMacro,
-					strReBegin,
-					strReEnd;
-		std::stringstream ss;
-		for(;;) {
-			if(def.eof())
-				break;
-			std::getline(def, strMacro);		// マクロ名
-			std::getline(def, strReBegin);		// RegEx構文(Begin)
-			std::getline(def, strReEnd);		// RegEx構文(End)
-			if(strMacro.empty() || strReBegin.empty() || strReEnd.empty())
-				break;
-			regex re_begin(strReBegin),
-					re_end(strReEnd);
-			for(;;) {
-				// 定義ファイルの記述に合った箇所まで読み飛ばし -> (読み込み始める地点)
-				if(!regex_search(itr, itrE, res, re_begin))
-					break;
-				itr = res.suffix().first;
-
-				// 定義の終わりを検出 -> (読み込み終わりの地点)
-				if(!regex_search(itr, itrE, res, re_end))
-					return 1;
-
-				bool bDef = true;
-				auto itrLE = res.suffix().first;
-				for(;;) {
-					// プロトタイプ宣言の名前を取得
-					if(!regex_search(itr, itrLE, res, re_proto))
-						break;
-					itr = res.suffix().first;
-
-					// 既に出力した定義名ならばスキップ
-					Func func;
-					func.name = res.str(2);
-					func.ret_type = res.str(1);
-					if(g_bAppend && funcsInFile.count(func.name)!=0) {
-						++skipcount;
-						continue;
-					}
-					++count;
-					if(bDef) {
-						bDef = false;
-						// マクロ名を書き出す
-						ofs << "#ifdef " << strMacro << std::endl;
-					}
-
-					funcsInFile.insert(func.name);
-					{
-						auto itr = res[3].first,
-							itrE = res[3].second;
-						smatch res2;
-						while(regex_search(itr, itrE, res2, re_args)) {
-							func.arg_pair.push_back(ArgPair{res2.str(1), res2.str(2)});
-							itr = res2.suffix().first;
-						}
-					}
-
-					ss.str("");
-					ss.clear();
-					std::string funcDef;
-					funcDef.resize(func.name.size());
-					// タイプ定義の名前を作成
-					if(g_exp & Exp_Definition) {
-						// :大文字変換
-						std::transform(func.name.cbegin(), func.name.cend(), funcDef.begin(), ::toupper);
-						ss << "PFN" << funcDef << "PROC";
-						// タイプ定義のアウトプット
-						ofs << "GLDEFINE(" + func.name + ',' + ss.str() + ')' << std::endl;
-					}
-					// GLラッパー定義のアウトプット
-					if(g_exp & Exp_Method) {
-						// ret_typeがvoidなら0, それ以外は1を出力
-						int bVoid = (func.ret_type == "void") ? 0 : 1;
-						ofs << "DEF_GLMETHOD(" << func.ret_type << ", " << bVoid << ", " << func.name << ", ";
-						if(func.arg_pair.empty())
-							ofs << "(), ()";
-						else {
-							if(func.arg_pair[0].type_name == "void")
-								ofs << "(), ()";
-							else {
-								for(auto& ap : func.arg_pair)
-									ofs << "(" << ap.type_name << ")";
-								ofs << ", ";
-								for(auto& ap : func.arg_pair)
-									ofs << "(" << ap.name << ")";
-							}
-						}
-						ofs << ')' << std::endl;
-					}
-				}
-				if(!bDef)
-					ofs << "#endif" << std::endl;
-			}
+		ExportFuncV fl;
+		if(g_exp & Exp_Definition) {
+			// タイプ定義の名前を作成
+			fl.emplace_back(&ExportDefinition);
 		}
+		if(g_exp & Exp_Method) {
+			// GLラッパー定義のアウトプット
+			fl.emplace_back(&ExportMethod);
+		}
+		if(!fl.empty())
+			count += IterateFunctionDefinition(ofs, def, buff, fl);
+
 		// スキップした数と出力された数を表示
-		std::cout << count << " entries exported." << std::endl;
-		std::cout << skipcount << " entries skipped." << std::endl;
+		std::cout << count.count << " entries exported." << std::endl;
+		std::cout << count.skipcount << " entries skipped." << std::endl;
 	} catch(const std::exception& e) {
 		std::cout << "error occured!" << std::endl << e.what() << std::endl;
 	}
